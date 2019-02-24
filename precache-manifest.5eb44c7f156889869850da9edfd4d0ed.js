400be1545de8d45fc023a910a3532f528d228e8a
Yobany <yobanydesign@gmail.com>
2019-02-23 23:38:34 -0500

    Updates

---
+++ precache-manifest.5eb44c7f156889869850da9edfd4d0ed.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "7985e359b3121cf81761",
+    "url": "/clicky-game/static/js/runtime~main.7985e359.js"
+  },
+  {
+    "revision": "792ad5eb1f58810e5c8b",
+    "url": "/clicky-game/static/js/main.792ad5eb.chunk.js"
+  },
+  {
+    "revision": "f51302ea4a2fe62c4004",
+    "url": "/clicky-game/static/js/2.f51302ea.chunk.js"
+  },
+  {
+    "revision": "792ad5eb1f58810e5c8b",
+    "url": "/clicky-game/static/css/main.7dd8c8be.chunk.css"
+  },
+  {
+    "revision": "10867042528c35ac4314451b94382f92",
+    "url": "/clicky-game/index.html"
+  }
+];
